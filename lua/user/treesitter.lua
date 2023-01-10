5566d21fc84e99a1db28873a25c93b186af944a6
Rafael Elias <rafaelniza2000@hotmail.com>
2023-01-10 11:08:53 -0300

    add new ensure installed languages, enable the sync install and disable the highlight for rust

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -9,14 +9,14 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "go", "rust" }, -- put the language you want in this array
   -- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
+	sync_install = true, -- install languages synchronously (only applied to `ensure_installed`)
+
   highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = {}, -- list of language that will be disabled
+		disable = { "rust" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
